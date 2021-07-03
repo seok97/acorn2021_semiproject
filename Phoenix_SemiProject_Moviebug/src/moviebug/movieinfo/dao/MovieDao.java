723223c3d903c76f4777bddc615f65d6e68caa3f
hs_keko <manic9075@gmail.com>
2021-07-04 02:12:21 +0900

    MovieDao 에 getData 메소드 생성

--- Phoenix_SemiProject_Moviebug/src/moviebug/movieinfo/dao/MovieDao.java
+++ Phoenix_SemiProject_Moviebug/src/moviebug/movieinfo/dao/MovieDao.java
@@ -22,6 +22,56 @@ public class MovieDao {
 	}
 	
 	
+	//영화 하나의 정보를 리턴하는 메소드
+	public MovieDto getData(int num) {
+		MovieDto dto=null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			//Connection 객체의 참조값 얻어오기 
+			conn = new DbcpBean().getConn();
+			//실행할 sql 문 작성
+			String sql = "SELECT movie_num,movie_title_kr ,movie_title_eng ,movie_story,movie_character,movie_year,movie_genre,movie_company,movie_image,movie_trailer,movie_time,movie_rating"
+					+ " FROM movie_info"
+					+ " WHERE movie_num=?";
+			//PreparedStatement 객체의 참조값 얻어오기
+			pstmt = conn.prepareStatement(sql);
+			//? 에 바인딩할 내용이 있으면 여기서 바인딩
+			pstmt.setInt(1, num);
+			//select 문 수행하고 결과를 ResultSet 으로 받아오기
+			rs = pstmt.executeQuery();
+			//반복문 돌면서 ResultSet 객체에 있는 내용을 추출해서 원하는 Data type 으로 포장하기
+			if (rs.next()) {
+				dto = new MovieDto();
+				dto.setMovie_num(num);
+				dto.setMovie_title_kr(rs.getString("movie_title_kr"));
+				dto.setMovie_title_eng(rs.getString("movie_title_eng"));
+				dto.setMovie_story(rs.getString("movie_story"));
+				dto.setMovie_character(rs.getString("movie_character"));
+				dto.setMovie_year(rs.getString("movie_year"));
+				dto.setMovie_genre(rs.getString("movie_genre"));
+				dto.setMovie_company(rs.getString("movie_company"));
+				dto.setMovie_image(rs.getString("movie_image"));
+				dto.setMovie_trailer(rs.getString("movie_trailer"));
+				dto.setMovie_time(rs.getString("movie_time"));
+				dto.setMovie_rating(rs.getString("movie_rating"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return dto;
+	}
 	
 	public boolean insert(MovieDto dto) {
 		Connection conn = null;
