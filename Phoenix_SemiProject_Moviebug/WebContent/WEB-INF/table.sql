75f7e081b5a6bed4bbf2024bf9585d4b0a4350c4
hs_keko <manic9075@gmail.com>
2021-07-05 01:25:00 +0900

    table.sql 수정

--- Phoenix_SemiProject_Moviebug/WebContent/WEB-INF/table.sql
+++ Phoenix_SemiProject_Moviebug/WebContent/WEB-INF/table.sql
@@ -52,7 +52,7 @@ CREATE SEQUENCE board_comment_seq;
 
 -- 영화 정보를 저장할 테이블
 CREATE TABLE movie_info(
-	movie_num NUMBER PRIMARY KEY, --영화 번호
+	movie_num NUMBER PRIMARY KEY not null, --영화 번호
 	movie_title_kr VARCHAR2(100), --영화 제목(한글)
 	movie_title_eng VARCHAR2(100), --영화 제목(영문)
 	movie_story VARCHAR2(2000), -- 영화 줄거리
@@ -63,7 +63,9 @@ CREATE TABLE movie_info(
 	movie_image VARCHAR2(1000), -- 영화 이미지 경로
 	movie_trailer VARCHAR2(1000), -- 영화 트레일러 경로
 	movie_time VARCHAR2(10), -- 러닝 타임
-	movie_rating VARCHAR2(10) -- 영화 평점
+	movie_rating VARCHAR2(10), -- 영화 평점
+	movie_nation VARCHAR2(25), -- 제작 국가
+	movie_director VARCHAR2(20) -- 감독
 );
 
 -- 영화 정보 번호를 얻어낼 시퀀스
