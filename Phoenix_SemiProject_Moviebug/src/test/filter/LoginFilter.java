4974461e12e5457c76a7a3da73ef42012619db5d
chung <cndrms46@naver.com>
2021-07-05 13:56:27 +0900

    loginFilter 수정

--- Phoenix_SemiProject_Moviebug/src/test/filter/LoginFilter.java
+++ Phoenix_SemiProject_Moviebug/src/test/filter/LoginFilter.java
@@ -48,8 +48,8 @@ public class LoginFilter implements Filter{
       //1. 로그인을 했는지 로그인된 아이디를 읽어와 본다. (HttpSession 객체)
       HttpServletRequest req=(HttpServletRequest)request;
       HttpSession session=req.getSession();
-      String id=(String)session.getAttribute("id");
-      if(id != null) {
+      String email=(String)session.getAttribute("email");
+      if(email != null) {
          //2. 만일 로그인을 했다면 관여 하지 않고 요청의 흐름을 이어간다. (FilterChain 객체)
          chain.doFilter(request, response);
       }else {
